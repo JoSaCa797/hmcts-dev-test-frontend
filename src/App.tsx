730e828105be4404d9a015dac7331db8d66c5afb
jc797 <jc797@student.le.ac.uk>
2025-08-09 15:51:07 +0100

    Updated to latest version of taiwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { useSelector } from "react-redux"
-import { type RootState } from "./redux/store"
+import UserTaskTable from "./components/UserTaskTable"
 
 function App() {
-    
-    const userTasks = useSelector((state: RootState) => state.userTasks)
-
     return (
-        <div className="w-screen h-screen bg-pink-300">
-            {
-                userTasks.map((ut, index) => {
-                    return (
-                        <p key={index}>{ut.Title}, {ut.Description}, {ut.Status}, {ut.Due.toString()}</p>
-                    )
-                })
-            }
+        <div className="w-screen h-screen bg-slate-600">
+            <p>asokdas</p>
+            <UserTaskTable />
         </div>
     )
 }
---
+++ src/components/UserTaskTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserTaskTable = () => {
+    return (
+        <div>UserTaskTable</div>
+    )
+}
+
+export default UserTaskTable
